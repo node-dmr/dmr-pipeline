6cf4bcf84f0e84637ecfa345c0535f4b2ea183a0
qiansc <van3008@163.com>
2018-09-16 02:46:58 +0800

    upgrade to ts project

--- test-tmp/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __cov_zvN8EGjaWGP3R_FkfHAHPA = (Function('return this'))();
-if (!__cov_zvN8EGjaWGP3R_FkfHAHPA.$$cov_1537032008355$$) { __cov_zvN8EGjaWGP3R_FkfHAHPA.$$cov_1537032008355$$ = {}; }
-__cov_zvN8EGjaWGP3R_FkfHAHPA = __cov_zvN8EGjaWGP3R_FkfHAHPA.$$cov_1537032008355$$;
-if (!(__cov_zvN8EGjaWGP3R_FkfHAHPA['/mnt/d/dmr/dmr-pipeline/dist/src/index.js'])) {
-   __cov_zvN8EGjaWGP3R_FkfHAHPA['/mnt/d/dmr/dmr-pipeline/dist/src/index.js'] = {"path":"/mnt/d/dmr/dmr-pipeline/dist/src/index.js","s":{"1":0,"2":0,"3":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":2,"column":0},"end":{"line":2,"column":62}},"2":{"start":{"line":9,"column":0},"end":{"line":9,"column":41}},"3":{"start":{"line":10,"column":0},"end":{"line":10,"column":42}}},"branchMap":{}};
-}
-__cov_zvN8EGjaWGP3R_FkfHAHPA = __cov_zvN8EGjaWGP3R_FkfHAHPA['/mnt/d/dmr/dmr-pipeline/dist/src/index.js'];
-__cov_zvN8EGjaWGP3R_FkfHAHPA.s['1']++;Object.defineProperty(exports,'__esModule',{value:true});__cov_zvN8EGjaWGP3R_FkfHAHPA.s['2']++;var transform_1=require('./transform');__cov_zvN8EGjaWGP3R_FkfHAHPA.s['3']++;exports.Transform=transform_1.Transform;
